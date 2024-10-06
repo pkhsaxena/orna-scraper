ff710137230fe49726e897672d04ccd55f462ab1
Prakhar Saxena <saxenaprakhar1998@gmail.com>
2024-10-05 20:12:11 -0400

    added code to check for next button, removed page save to file

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import requests, time, json
 from bs4 import BeautifulSoup
 
-def fetch_and_save_html(url, file_name):
+def fetch_and_parse(url)-> bool:
     headers={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:131.0) Gecko/20100101 Firefox/131.0'}    
     response = requests.get(url, headers=headers)
     
     if response.status_code == 200:
-        with open(file_name, 'w', encoding='utf-8') as file:
-            file.write(response.text)
+            content = "".join(line.strip() for line in response.text.split('\n'))
+            return parse(content)
     else:
-        print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+        raise Exception(f"Failed to retrieve the webpage. Status code: {response.status_code}")
 
-def parse(file_name):
-    with open(file_name, 'r', encoding='utf-8') as file:
-        content = file.read()
-        content = "".join(line.strip() for line in content.split('\n'))
-
+def parse(content) -> bool:
     soup = BeautifulSoup(content, 'html.parser')
 
     # can also do { "class": "codex-entries-entry" }
     items = soup.find_all('a', class_='codex-entries-entry')
-
+    
     for item in items:
         details = {}
         details['name']=item.contents[1].contents[0]
@@ -31,19 +27,22 @@ def parse(file_name):
         with open('temp.dump','a', encoding='utf-8') as codex:
             codex.write(json_data)
             codex.write('\n')
+    
+    pagination = soup.find('div', class_='pagination')
+    return pagination is not None and "Next" in pagination.text
 
 if __name__ == "__main__":
-    for i in range(1,62):
-        url = f'https://playorna.com/codex/items/?p={i}'
-        print(f'Requesting page {i}')
-        file_name = 'page.html'
-
-        fetch_and_save_html(url, file_name)
-        parse(file_name)
+    page=1
+    url = f'https://playorna.com/codex/items/?p={page}'
+    print(f'Requesting page {page}')
+    while(fetch_and_parse(url)):
         time.sleep(10)
+        page += 1
+        print(f"Requesting page {page}")
+        url = f'https://playorna.com/codex/items/?p={page}'
 
     codex_items=[]
-    with open('temp.dump','r',encoding='utf-8') as content, open('codex.json', 'w', encoding='utf-8') as codex:
+    with open('temp.dump','r',encoding='utf-8') as content, open('codex.txt', 'w', encoding='utf-8') as codex:
         [codex_items.append(json.loads(line)) for line in content]
         codex.write(json.dumps(codex_items,indent=4))
     
